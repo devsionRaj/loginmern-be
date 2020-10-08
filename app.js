4671771d28b31791d7633745f529d44ca5dd0263
Raj Kumar Das <devsionraj@hotmail.com>
2020-10-08 09:23:14 +0530

    Signup route created successfully

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 require('dotenv').config()
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/auth');
 
 var app = express();
 
